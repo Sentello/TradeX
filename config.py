351dfa3585f4d29b6847730bfa117fbaab3de17c
Sentello <44606412+Sentello@users.noreply.github.com>
2024-11-28 12:35:32 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+# Leave blank if not using
+
+# Replace with your secure PIN
+WEBHOOK_PIN = "123456" 
+
+# Bybit API credentials
+BYBIT_API_KEY = ""
+BYBIT_API_SECRET = ""
+
+# Binance API credentials
+BINANCE_API_KEY = ""
+BINANCE_API_SECRET = ""
